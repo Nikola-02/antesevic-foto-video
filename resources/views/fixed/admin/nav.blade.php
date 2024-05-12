3538536a92146d6311c6ec065894b6faa67750cf
Nikola <nikoladjunisic@gmail.com>
2024-05-12 14:42:39 +0200

    Added navigation links

--- resources/views/fixed/admin/nav.blade.php
+++ resources/views/fixed/admin/nav.blade.php
@@ -14,14 +14,38 @@
             <ul id="sidebarnav">
                 <li class="nav-small-cap">
                     <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-                    <span class="hide-menu">Admin</span>
+                    <span class="hide-menu">Admin pages</span>
                 </li>
                 <li class="sidebar-item">
-                    <a class="sidebar-link" href="./index.html" aria-expanded="false">
+                    <a class="sidebar-link" href="/admin" aria-expanded="false">
                                 <span>
                                   <i class="ti ti-layout-dashboard"></i>
                                 </span>
                         <span class="hide-menu">Dashboard</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="/" aria-expanded="false">
+                                <span>
+                                  <i class="ti ti-category-2"></i>
+                                </span>
+                        <span class="hide-menu">Kategorije (Glavno)</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="/" aria-expanded="false">
+                                <span>
+                                  <i class="ti ti-album"></i>
+                                </span>
+                        <span class="hide-menu">Galerije</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="/" aria-expanded="false">
+                                <span>
+                                  <i class="ti ti-photo-star"></i>
+                                </span>
+                        <span class="hide-menu">Postovi</span>
                     </a>
                 </li>
                 <li class="nav-small-cap">
