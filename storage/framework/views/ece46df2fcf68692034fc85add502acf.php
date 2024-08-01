c0b7a4aa0d095eb341a6e5375d789a0e385895da
Nikola-02 <nikoladjunisic@gmail.com>
2024-08-01 20:18:06 +0200

    Added post controller

--- storage/framework/views/ece46df2fcf68692034fc85add502acf.php
+++ storage/framework/views/ece46df2fcf68692034fc85add502acf.php
@@ -29,7 +29,7 @@
 
                 <?php if(session('wrong_credentials')): ?>
                 <div class="bg-red-100 border border-red-400 text-red-700 px-5 py-3 rounded relative" role="alert">
-                    <span class="block sm:inline"><?php echo e(session('wrong_credentials')); ?></span>
+                    <span class="block sm:inline text-base"><?php echo e(session('wrong_credentials')); ?></span>
                 </div>
                 <?php endif; ?>
 
