e219bee4b8acf325124c69f73a0d38f45a81d611
Nikola-02 <nikoladjunisic@gmail.com>
2024-08-14 23:43:24 +0200

    Added css for card post

--- resources/views/pages/admin/galleries/show.blade.php
+++ resources/views/pages/admin/galleries/show.blade.php
@@ -18,37 +18,104 @@
                             </a>
                         </div>
 
-                        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                            <a href="/admin/posts/createForGallery/{{ $gallery->id }}">
-                                <img class="rounded-t-lg" src="{{asset('assets/images/galleries/' . '1721772383_mic-6127818_1280.jpg')}}" alt="" />
-                            </a>
-                        </div>
-                        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <div class="shadow-xl max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                             <a href="#">
                                 <img class="rounded-t-lg" src="{{asset('assets/images/galleries/' . '1721772383_mic-6127818_1280.jpg')}}" alt="" />
                             </a>
+
+                            <div class="flex items-center p-3 justify-between">
+                                <div class="title-post flex flex-wrap">
+                                    <p class="mr-2 font-semibold">Created at:</p>
+                                    <p>10/05/2020 18:30:55</p>
+                                </div>
+
+                                <div class="buttons-post flex items-center gap-2">
+                                    <span class="border-bottom-0">
+                                    <form action="/admin/categories/2" method="post">
+                                        @csrf
+                                        @method('delete')
+                                        <button type="submit" class="btn btn-danger mb-0"><i class="ti ti-trash"></i></button>
+                                    </form>
+                                </span>
+                                </div>
+
+                            </div>
+
                         </div>
 
-                        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <div class="shadow-xl max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                             <a href="#">
                                 <img class="rounded-t-lg" src="{{asset('assets/images/galleries/' . '1721772383_mic-6127818_1280.jpg')}}" alt="" />
                             </a>
+
+                            <div class="flex items-center p-3 justify-between">
+                                <div class="title-post flex flex-wrap">
+                                    <p class="mr-2 font-semibold">Created at:</p>
+                                    <p>10/05/2020 18:30:55</p>
+                                </div>
+
+                                <div class="buttons-post flex items-center gap-2">
+                                    <span class="border-bottom-0">
+                                    <form action="/admin/categories/2" method="post">
+                                        @csrf
+                                        @method('delete')
+                                        <button type="submit" class="btn btn-danger mb-0"><i class="ti ti-trash"></i></button>
+                                    </form>
+                                </span>
+                                </div>
+
+                            </div>
+
                         </div>
 
-                        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <div class="shadow-xl max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                             <a href="#">
                                 <img class="rounded-t-lg" src="{{asset('assets/images/galleries/' . '1721772383_mic-6127818_1280.jpg')}}" alt="" />
                             </a>
-                            <span class="border-bottom-0">
-                                <a href="/admin/categories/2/edit" class="btn btn-primary mb-0"><i class="ti ti-pencil"></i></a>
-                            </span>
-                            <span class="border-bottom-0">
-                                <form action="/admin/categories/2" method="post">
-                                    @csrf
-                                    @method('delete')
-                                    <button type="submit" class="btn btn-danger mb-0"><i class="ti ti-trash"></i></button>
-                                </form>
-                            </span>
+
+                            <div class="flex items-center p-3 justify-between">
+                                <div class="title-post flex flex-wrap">
+                                    <p class="mr-2 font-semibold">Created at:</p>
+                                    <p>10/05/2020 18:30:55</p>
+                                </div>
+
+                                <div class="buttons-post flex items-center gap-2">
+                                    <span class="border-bottom-0">
+                                    <form action="/admin/categories/2" method="post">
+                                        @csrf
+                                        @method('delete')
+                                        <button type="submit" class="btn btn-danger mb-0"><i class="ti ti-trash"></i></button>
+                                    </form>
+                                </span>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                        <div class="shadow-xl max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                            <a href="#">
+                                <img class="rounded-t-lg" src="{{asset('assets/images/galleries/' . '1721772383_mic-6127818_1280.jpg')}}" alt="" />
+                            </a>
+
+                            <div class="flex items-center p-3 justify-between">
+                                <div class="title-post flex flex-wrap">
+                                    <p class="mr-2 font-semibold">Created at:</p>
+                                    <p>10/05/2020 18:30:55</p>
+                                </div>
+
+                                <div class="buttons-post flex items-center gap-2">
+                                    <span class="border-bottom-0">
+                                    <form action="/admin/categories/2" method="post">
+                                        @csrf
+                                        @method('delete')
+                                        <button type="submit" class="btn btn-danger mb-0"><i class="ti ti-trash"></i></button>
+                                    </form>
+                                </span>
+                                </div>
+
+                            </div>
+
                         </div>
 
                     </div>
